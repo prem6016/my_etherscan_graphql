e732c057e014544a8b12e6d36cabc4fd5f4dc537
Prem Sarkar <premsarkardps@gmail.com>
2023-11-23 20:43:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+const { ApolloServer } = require("apollo-server");
+// Import the GraphQL schema from the schema file
+const { importSchema } = require("graphql-import");  
+const EtherDataSource = require("./datasource/ethDatasource");
+const typeDefs = importSchema("./schema.graphql"); 
+
+require("dotenv").config();
+
+const resolvers = {
+  Query: {
+    // Resolver to get ether balance for an address
+    etherBalanceByAddress: (root, _args, { dataSources }) =>  
+      dataSources.ethDataSource.etherBalanceByAddress(),
+
+    // Resolver to get total ether supply
+    totalSupplyOfEther: (root, _args, { dataSources }) =>
+      dataSources.ethDataSource.totalSupplyOfEther(),
+
+    // Resolver to get latest ETH price
+    latestEthereumPrice: (root, _args, { dataSources }) =>
+      dataSources.ethDataSource.getLatestEthereumPrice(),
+
+    // Resolver to get average block confirmation time
+    blockConfirmationTime: (root, _args, { dataSources }) =>
+      dataSources.ethDataSource.getBlockConfirmationTime(),
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources: () => ({
+    // Instantiate EtherDataSource
+    ethDataSource: new EtherDataSource(),  
+  }), 
+});
+
+server.timeout = 0;
+server.listen("9000").then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
